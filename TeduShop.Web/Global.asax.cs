24cd0200492f3170c2fc2f486fd583a4854520e2
DESKTOP-UAQ1PI3\Quoc Tran <tranvanquocvq@gmail.com>
2018-08-12 20:30:35 +0700

    add asp.net indentity

--- TeduShop.Web/Global.asax.cs
+++ TeduShop.Web/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using TeduShop.Web.Mappings;
 namespace TeduShop.Web
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -15,6 +15,7 @@ namespace TeduShop.Web
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            AutoMapperConfiguration.Configure();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
